7ae08954b42b7e3caecd6a0e12725e4030278166
Giuseppe Latini <giuseppe.latini@gmail.com>
2024-08-27 10:00:20 +0200

    Caricamento iniziale

---
+++ tests/test_ittc.py
@@ -0,0 +1,16 @@
+from src.ittaxcode_pinuxone import ittc as cf
+
+
+def test():
+
+    assert cf.check(
+        "PPPNNNLMBMQLMMTA"
+    )
+
+    assert cf.age(
+        "PPPNNNLMBMQLMMTA"
+    ) == 23
+
+    assert str(cf.birthdate(
+        "PPPNNNLMBMQLMMTA"
+    )) == "2001-02-14"
